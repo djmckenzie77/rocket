5bf95dab663c6ecc9335f3c307498110f97ec58b
Gleison Storto <gleisonstorto@gmail.com>
2017-12-18 01:51:58 -0200

    - Did some tests with bftee, finally mbuffer seems to be the optimal solution - Some tests of router bandwidth were added. Optimal channel seems to be #1

--- launch.bash
+++ launch.bash
@@ -4,8 +4,8 @@
 current_date=$(date '+_%Y%m%d_%H%M%S')
 remote_ssh="pi@192.168.0.100"
 remote_script="remote.bash"
-remote_ftee_c="ftee.c"
-remote_ftee="ftee"
+multipipe="ftee"
+fps="30"
 
 function clean_host {
     # kill all applications PC-side
@@ -18,8 +18,8 @@ function clean_host {
 function clean_remote {
     ssh $remote_ssh << EOF
         rm -f $remote_script
-        rm -f $remote_ftee_c
-        rm -f $remote_ftee
+        rm -f ${multipipe}.c
+        rm -f ${multipipe}
         rm -f ./netcat_fifo
         sudo killall raspivid 
         sudo killall netcat
@@ -28,14 +28,16 @@ EOF
 
 clean_host
 clean_remote
-netcat -l -p 5000 | mplayer -vf scale -zoom -xy 1280 -fps 60 -cache-min 50 -cache 2048 - &
+netcat -l -p 5000 | mplayer -vf scale -zoom -xy 1280 -fps $fps -cache-min 50 -cache 1024 - &
 # copy remote files to RPi
 scp $remote_script $remote_ssh:
-scp $remote_ftee_c $remote_ssh:
+scp ${multipipe}.c $remote_ssh:
 # set trap for raspivid
 trap clean_remote SIGINT
 ssh $remote_ssh << EOF
   export REMOTE_DATE=$current_date
+  export MULTIPIPE=$multipipe
+  export FPS=$fps
   nohup bash -x remote.bash &> remote${current_date}.log < /dev/null &
 EOF
 sleep infinity
--- remote.bash
+++ remote.bash
@@ -2,11 +2,12 @@
 # kill all applications RPi-side
 sudo killall -q raspivid
 sudo killall -q netcat
-# creates ftee bin
-gcc ftee.c -o ftee
+# creates multipipe bin
+gcc ${MULTIPIPE}.c -o ${MULTIPIPE}
 # start streaming to PC
 video_out="video${REMOTE_DATE}.h264"
 netcat_log="netcat${REMOTE_DATE}.log"
+mbuffer_log="mbuffer${REMOTE_DATE}.log"
 mkfifo netcat_fifo
-raspivid -t 0 -w 1920 -h 1080 -o - | ./ftee netcat_fifo > $video_out &
-netcat -v 192.168.0.101 5000 < netcat_fifo &> $netcat_log &
+raspivid -t 0 -md 5 -fps $FPS -o - | ./${MULTIPIPE} netcat_fifo > $video_out &
+cat netcat_fifo | mbuffer --direct -t -s 2k 2> $mbuffer_log | netcat -v 192.168.0.101 5000 &> $netcat_log &
