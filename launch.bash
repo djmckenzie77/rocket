27cc1ae7bbbb13d7b91fd6de3a6f4289425d86f3
Gleison Storto <gleisonstorto@gmail.com>
2017-12-19 16:49:48 -0200

    - Removed ftee from launch.bash - Separated raspivid options from mbuffer/netcat

--- launch.bash
+++ launch.bash
@@ -5,7 +5,6 @@ current_date=$(date '+_%Y%m%d_%H%M%S')
 remote_ssh="pi@192.168.0.100"
 remote_script="remote.bash"
 remote_dir="rocket"
-multipipe="ftee"
 fps="30"
 
 function clean_host {
@@ -19,8 +18,6 @@ function clean_host {
 function clean_remote {
     ssh $remote_ssh << EOF
         rm -f ${remote_dir}/${remote_script}
-        rm -f ${remote_dir}/${multipipe}.c
-        rm -f ${remote_dir}/${multipipe}
         rm -f ${remote_dir}/netcat_fifo
         sudo killall raspivid 
         sudo killall netcat
@@ -32,12 +29,10 @@ clean_remote
 netcat -l -p 5000 | mplayer -vf scale -zoom -xy 1280 -fps $fps -cache-min 50 -cache 1024 - &
 # copy remote files to RPi
 scp $remote_script $remote_ssh:~/${remote_dir}
-scp ${multipipe}.c $remote_ssh:~/${remote_dir}
 # set trap for raspivid
 trap clean_remote SIGINT
 ssh $remote_ssh << EOF
   export REMOTE_DATE=$current_date
-  export MULTIPIPE=$multipipe
   export FPS=$fps
   cd $remote_dir
   nohup bash -x remote.bash &> remote${current_date}.log < /dev/null &
--- remote.bash
+++ remote.bash
@@ -1,15 +1,22 @@
 #!/bin/bash
+
 # kill all applications RPi-side
 sudo killall -q raspivid
 sudo killall -q netcat
-# creates multipipe bin
-gcc ${MULTIPIPE}.c -o ${MULTIPIPE}
-# start streaming to PC
+
+# raspivid options
+raspivid_bin="/home/pi/raspberry/build/bin/raspivid"
+raspivid_log="raspivid${REMOTE_DATE}.log"
+# -br = brightness [0 to 100] -co = contrast [-100 to 100]; -sh = sharpness [-100 to 100]
+raspivid_opt="-md 5 -fps $FPS"
+
+# raspivid |-> FIFO (non-blocking)
+#          |-> $video_out
+mkfifo netcat_fifo
 video_out="video${REMOTE_DATE}.h264"
+$raspivid_bin -t 0 $raspivid_opt -o $video_out -o2 netcat_fifo &> $raspivid_log &
+
+# FIFO (non-blocking) -> netcat -> receiver
 netcat_log="netcat${REMOTE_DATE}.log"
 mbuffer_log="mbuffer${REMOTE_DATE}.log"
-raspivid_log="raspivid${REMOTE_DATE}.log"
-raspivid_bin="/home/pi/raspberry/build/bin/raspivid"
-mkfifo netcat_fifo
-$raspivid_bin -t 0 -md 5 -fps $FPS -o $video_out -o2 netcat_fifo &> $raspivid_log &
 cat netcat_fifo | mbuffer -s 2k 2> $mbuffer_log | netcat -v 192.168.0.101 5000 &> $netcat_log &
