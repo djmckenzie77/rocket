f8eccbd24ad8011d015527c0b2c39e59511c0b9e
Gleison Storto <gleisonstorto@gmail.com>
2017-12-18 22:49:52 -0200

    Using custom raspivid with non-blocking buffer

--- remote.bash
+++ remote.bash
@@ -8,6 +8,7 @@ gcc ${MULTIPIPE}.c -o ${MULTIPIPE}
 video_out="video${REMOTE_DATE}.h264"
 netcat_log="netcat${REMOTE_DATE}.log"
 mbuffer_log="mbuffer${REMOTE_DATE}.log"
+raspivid_bin="/home/pi/raspberry/build/bin/raspivid"
 mkfifo netcat_fifo
-raspivid -t 0 -md 5 -fps $FPS -o - | ./${MULTIPIPE} netcat_fifo > $video_out &
+$raspivid_bin -t 0 -md 5 -fps $FPS -o $video_out -o2 netcat_fifo &
 cat netcat_fifo | mbuffer --direct -t -s 2k 2> $mbuffer_log | netcat -v 192.168.0.101 5000 &> $netcat_log &
